6262cd1180a56f3ee246edd1f752772afe7b1d9e
ericdjadja <ericdjadja@gmail.com>
2023-06-15 18:17:29 -0700

    add 6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,10 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of char: %d\n",sizeof(char));
+printf("Size of an int: %d\n",sizeof(int));
+printf("Size of a long int: %d\n",sizeof(unsigned int));
+printf("Size of a long long int: $d\n",sizeof(long));
+printf("Size of a float: %d\n",sizeof(float));
 return (0);
 }
