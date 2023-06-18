f905f6a32ae961dddfa60ab3bac7a3aa26be401a
eric djadja <ericdjadja@gmail.com>
2023-06-18 00:56:22 +0000

    add 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ if (n % 10 == 0)
 if (n % 10 < 6)
 {
 	d = n % 10;
-	printf("Last digit of %d is %d is less than 6 and not 0\n", n,d);
-
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n,d);
+}
 return (0);
 }
