84fbc0241ac5fbcb06e178fa0ff9f6587d54d0e0
eric djadja <ericdjadja@gmail.com>
2023-06-20 23:23:03 +0000

    add _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *out = "_putchar";
+printf("%s\n", out);
+return (0);
+}
