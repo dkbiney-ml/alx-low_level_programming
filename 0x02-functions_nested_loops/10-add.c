29c318a75e4a6693678c344cd1820e02a273f6e7
eric djadja <ericdjadja@gmail.com>
2023-06-21 11:53:37 +0000

    add 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - prints the sign of a number
+ * @n : is the digit input
+ * @d : sdfsgsgfs
+ * Return: 1 for positive, -1 for negative and 0 for 0 (Success)
+*/
+int add(int n, int d)
+{
+return (n + d);
+}
