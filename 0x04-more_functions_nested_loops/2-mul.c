c299f8717fc4e504d7e631c34c53f66ff25aea9e
eric djadja <ericdjadja@gmail.com>
2023-06-24 16:10:14 +0000

    add 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a: The character to print
+ * @b: the second num
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int sum = a * b;
+return (sum);
+}
--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-int c = 5;
-char i;
-for (i = 0; i <= 9; i++)
-{
-if (c == i)
-{
-putchar('0' + c);
-return (1);
-}
-}
-return (0);
+int a = 5;
+int b = 8;
+int sum = a * b;
+printf("%d",sum);
+return (sum);
 }
