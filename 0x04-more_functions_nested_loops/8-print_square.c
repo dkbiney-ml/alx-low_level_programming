ba92e2138f42e7bc417fa7bec2b0100c0bc2fa43
eric djadja <ericdjadja@gmail.com>
2023-06-24 17:56:34 +0000

    add 1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - writes the character c to stdout
+ * @size : number of a
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_square(int size)
+{
+int i = 0;
+while (i < size)
+{
+int j = 0;
+while (j < size)
+{
+_putchar('#');
+j++;
+}
+if (j != size - 1)
+_putchar('\n');
+i++;
+}
+_putchar('\n');
+}
