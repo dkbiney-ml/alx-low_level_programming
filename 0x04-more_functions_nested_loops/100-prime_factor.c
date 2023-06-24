9c739f6359da33c29fec3985f0231e23bc241a97
eric djadja <ericdjadja@gmail.com>
2023-06-24 20:08:38 +0000

    add 1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long st = 612852475143;
+long i;
+for (i = 2; i < 612852475143; i++)
+{
+while (st % i == 0)
+{
+st = st / i;
+}
+}
+printf("%ld", st);
+return (0);
+}
