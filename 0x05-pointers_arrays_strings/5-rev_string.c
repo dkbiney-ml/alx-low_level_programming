09d8b2c2603323be58127d796094a88d9556b4fb
eric djadja <ericdjadja@gmail.com>
2023-06-26 23:46:39 +0000

    add 6-puts

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,19 @@
 void print_rev(char *s)
 {
 int i = 0;
+int j;
 while (*(s + i) != '\0')
 {
 i++;
 }
-int j = i;
-char temp[i];
+j = i;
+char temp[j];
 while (i >= 1)
 {
 temp[i - 1] = *(s + (i - 1));
 i--;
 }
-while(i = 0)
+while(i == 0)
 {
 *(s + i) = temp[j - i - 1];
 }
