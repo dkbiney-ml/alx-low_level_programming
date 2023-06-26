729668527ec016f475a93e82b204e4c1927c8f71
eric djadja <ericdjadja@gmail.com>
2023-06-26 21:14:18 +0000

    add 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - entry point
+ * @str : input charact\
+ * Return : void
+*/
+void _puts(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar('\n');
+}
