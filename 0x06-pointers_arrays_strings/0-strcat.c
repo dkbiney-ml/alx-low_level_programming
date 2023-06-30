18183227bcd6f0064ad8626ab9cd65ce0df8ec25
eric djadja <ericdjadja@gmail.com>
2023-06-30 14:11:28 +0000

    add 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,10 +3,9 @@
  * _strcat - appends the src string to the dest string,
  *  overwriting the terminating null byte (\0) at the end of dest,
  *  and then adds a terminating null byte
- * 
  * @dest : input arr
  * @src : input arr
- * Return : ouput char arr
+ * Return: ouput char arr
 */
 char *_strcat(char *dest, char *src)
 {
@@ -22,6 +21,7 @@ dest[i] = src[j];
 if  (src[j] == '\0')
 break;
 i++;
+j++;
 }
 return (dest);
 }
