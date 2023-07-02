d644970f5d7cb1521fbd89fe1b2bf9de8ca0d5ab
eric djadja <ericdjadja@gmail.com>
2023-07-02 21:14:19 +0000

    add 101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_number - check the code
- * @str: an array of integers
+ * @n: an array of integers
  * Return: nothing.
  */
 void print_number(int n)
@@ -23,17 +23,17 @@ break;
 }
 d = n / max;
 
-if (d != 0 )
+if (d != 0)
 {
 _putchar('0' + d);
-zeroAllowed +=1;
+zeroAllowed += 1;
 }
 else
 {
 if (zeroAllowed)
 _putchar('0');
 }
-n -= d * max; 
+n -= d *max;
 max /= 10;
 }
 }
