4682275ac452246ab675e4d6981ee202e610a0a0
eric djadja <ericdjadja@gmail.com>
2023-07-02 18:22:57 +0000

    add 6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - check the code
+ * @a: an array of integers
+ * Return: nothing.
+ */
+char *cap_string(char *str)
+{
+int capitalizeNext = 1;
+while (*str)
+{
+if (*str == ' ' || *str == '\t' || *str == '\n' || 
+*str == '\r' || *str == '\f' || *str == '\v' || *str == '.' ||
+*str == ',' || *str == ';' || *str == '"' || *str == '!' ||
+*str == '?' || *str == ')' || *str == ')' || *str == '(' ||
+*str == '{' || *str == '}')
+{
+capitalizeNext = 1;
+}
+else
+{
+if (capitalizeNext && ((*str >= 'A' && *str <= 'Z') || (*str >= '0' && *str <= '9' )))
+{
+capitalizeNext = 0;
+}
+if (capitalizeNext && *str >= 'a' && *str <= 'z')
+{
+*str = *str - 'a' + 'A';
+capitalizeNext = 0;
+}
+}
+str++;
+}
+return (str);
+}
