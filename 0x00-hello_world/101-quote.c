02bfdae4c839d4656511c63b5186cb56e2058d57
eric djadja <ericdjadja@gmail.com>
2023-07-03 10:28:06 +0000

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
-
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+/**
+ * main - entry point
+ * Return: 1
+*/
+int main(void)
+{
+char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, m, sizeof(m) - 1);
+return (1);
 }
