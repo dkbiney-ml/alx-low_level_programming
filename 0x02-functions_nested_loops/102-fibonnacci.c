1b56de6352e1f5a1b14db7a0fa844e2ff18cab07
eric djadja <ericdjadja@gmail.com>
2023-07-04 09:09:00 +0000

    add 101-natural

---
+++ 0x02-functions_nested_loops/102-fibonnacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int prev1 = 1;
+int prev2 = 2;
+unsigned long cur;
+for (int i = 1; i <= 50; i++)
+{
+
+if (i == 1 || i == 2)
+printf("%d,", i);
+else
+{
+cur = prev1 + prev2;
+prev1 = prev2;
+prev2 = cur;
+printf("%lu,", cur);
+}
+if (i != 50)
+printf(" ");
+}
+return (0);
+}
