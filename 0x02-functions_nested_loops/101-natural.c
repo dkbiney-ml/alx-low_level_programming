e82a5147405755c197538a3ca39f96d37dde0dd3
eric djadja <ericdjadja@gmail.com>
2023-07-04 08:38:58 +0000

    add 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+int i = 0;
+int acc = 0;
+while (i < 1024)
+{
+if (!(i % 3) || !(i % 5))
+acc += i;
+i++;
+}
+printf("%d\n", acc);
+}
