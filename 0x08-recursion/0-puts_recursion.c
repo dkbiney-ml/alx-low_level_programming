8ed50b29e41a26727bfdfce4946178158eed5756
eric djadja <ericdjadja@gmail.com>
2023-07-06 00:44:54 +0000

    add 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion : entry
- * @s : char literal
- * Returns : 0
+*_puts_recursion - entry point of  the function.
+*@s : char literal
+*Returns : 0
 */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar('\n');    
+_putchar('\n');
 return;
 }
 _putchar(*s);
