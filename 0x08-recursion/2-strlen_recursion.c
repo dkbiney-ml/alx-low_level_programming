f84c06400861551a39ad5a2dae5a4272642e0218
eric djadja <ericdjadja@gmail.com>
2023-07-06 01:28:05 +0000

    add 1-print_rev_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen_recursion - entry point of  the function.
+*@s : char literal
+* Returns : 0
+*/
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if (*s == '\0')
+{
+return (n);
+}
+n = _strlen_recursion(s + 1);
+return (n + 1);
+}
