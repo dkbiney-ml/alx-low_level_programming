df01e4a67553248704d9f512ca4857cdf87e1711
eric djadja <ericdjadja@gmail.com>
2023-07-09 16:12:12 +0000

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry
+ *@argc: int
+ *@argv: char
+ *Return: int
+ */
+int main(int argc, char **argv)
+{
+int i;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", i);
+return (0);
+}
