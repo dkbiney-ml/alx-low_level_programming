b73042fa10b2168e7aaecf6fd6ccbb1adec03a6a
eric djadja <ericdjadja@gmail.com>
2023-07-09 15:57:51 +0000

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - entry
+ *@argc: int
+ *@argv: char
+ *Return: int
+ */
+int main(int argc, __attribute((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
