4eaad23faa9e39a73981fa7edcb2280cfa79f410
eric djadja <ericdjadja@gmail.com>
2023-07-11 16:54:14 +0000

    add 3-alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - entry
+ * @width: int
+ * @height: int
+ * Return: ptr
+*/
+int **alloc_grid(int width, int height)
+{
+int **grid;
+int i, j;
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+/*Free previously allocated memory*/
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+grid[i][j] = 0;
+}
+return (grid);
+}
