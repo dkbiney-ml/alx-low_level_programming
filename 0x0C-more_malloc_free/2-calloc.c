6a3e0c02d15e648a64bdca99f7b6c81b3cecd2f2
eric djadja <ericdjadja@gmail.com>
2023-07-14 23:28:18 +0000

    add 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *ptr;
 unsigned int total_size;
 if (nmemb == 0 || size == 0)
-return NULL;
+return (NULL);
 
 total_size = nmemb * size;
 ptr = malloc(total_size);
 if (ptr == NULL)
-return NULL;
+return (NULL);
 memset(ptr, 0, total_size);
-return ptr;
+return (ptr);
 }
