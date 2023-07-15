66209f10dd9925934f3f83066d538e4e272cfef7
eric djadja <ericdjadja@gmail.com>
2023-07-15 00:44:53 +0000

    add 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * _realloc - entry
+ * @ptr: ptr
+ * @old_size: int
+ * @new_size: int
+ * Return: ptr
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *p;
@@ -9,7 +16,7 @@ return (ptr);
 p = malloc(new_size);
 if (new_size > old_size)
 {
-strcpy(p, ptr);
+memcpy(p, ptr, old_size);
 free(ptr);
 return (p);
 }
@@ -18,6 +25,12 @@ if (new_size == 0 && ptr != NULL)
 free(ptr);
 return (NULL);
 }
-else
+if (ptr == NULL)
 return (p);
+else
+{
+memcpy(p, ptr, new_size);
+free(ptr);
+return (p);
+}
 }
