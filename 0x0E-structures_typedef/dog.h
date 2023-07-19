a34a080e512369bac54d6e67a482d4af5a9c2244
eric djadja <ericdjadja@gmail.com>
2023-07-19 12:44:46 +0000

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - yeh yeh
+ * @name: char
+ * @age: int
+ * @owner: char
+ * Description: woof
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
