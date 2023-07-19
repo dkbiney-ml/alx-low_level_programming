0423e161fddeff0dd763e28d5cf70d54e24aaa32
eric djadja <ericdjadja@gmail.com>
2023-07-19 13:37:20 +0000

    add inti_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - entry
  * @d: ptr
@@ -9,7 +10,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL)
+{
 d->age = age;
 d->name = name;
 d->owner = owner;
+}
 }
